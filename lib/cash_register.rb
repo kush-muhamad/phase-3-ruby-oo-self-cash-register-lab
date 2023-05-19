0dad84f9205ddfb642205f01703779b553e8b3dc
kush Muhamad <kushmohammed98@yahoo.com>
2023-05-19 23:41:09 +0300

    completed assignment

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,40 @@
+require 'pry'
 
+class CashRegister
+    attr_accessor :total, :discount
+    attr_reader 
+   
+    def initialize(discount = 0)
+        @discount= discount
+        @total = 0
+    end
+        
+        def add_item(title, price , amount = 1)
+            @total += price * amount
+            
+            
+        end
+
+        def apply_discount
+            if @discount > 0
+              my_discount = @total * @discount/100
+              @total = @total - my_discount
+               "After the discount, the total comes to $#{@total}."
+            else 
+                "There is no discount to apply."
+            end
+        end
+        def items(item, price)
+            
+            items << item
+        end
+end
+
+
+
+
+
+
+
+
+binding.pry
